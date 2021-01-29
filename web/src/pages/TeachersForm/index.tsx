281a36715aaac737dbbd9f3fe5bef9f8b86cae5e
deyvison uchoa <deyvisonuchoa@hotmail.com>
2021-01-29 11:11:05 -0300

    input component + teacherForm development

--- web/src/pages/TeachersForm/index.tsx
+++ web/src/pages/TeachersForm/index.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
 
 import '../../assets/styles/global.css';
+import Input from '../../components/input';
 import { PageHeader } from '../../components/PageHeader';
 import './styles.css';
 
 export default function TeachersForm() {
 
-    const tituloHeader:string = ""
+    const tituloHeader:string = "Que incrivel que voce queira dar aulas";
+    const description: string = "O primeiro passo é preencher esse formulário de inscrição";
 
     return (
         <>
+            <div id="page-teacher-form" className="container">
             <PageHeader 
-            title={tituloHeader}
+                title={tituloHeader}
+                description={description}
             />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input name="name" label="Nome Completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="whatsapp" label="Whatsapp"/>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Informações sobre a aula</legend>
+
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="cost" label="Custo da sua hora por aula"/>
+                </fieldset>
+            </main>
+            </div>
         </>
     )
 }
--- web/src/pages/TeachersList/index.tsx
+++ web/src/pages/TeachersList/index.tsx
@@ -6,6 +6,7 @@ import './styles.css';
 
 import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 import TeachersItem from '../../components/TeacherItem/intex';
+import Input from '../../components/input';
 
 export default function TeachersList() {
 
@@ -16,27 +17,15 @@ export default function TeachersList() {
             <div id="page-teacher-list" className="container">
                 <PageHeader title={tituloHeader} >
                     <form id="search-teachers">
-                        <div className="input-block">
-                            <label htmlFor="subject">Matéria</label>
-                            <input type="text" id="subject"/>
-                        </div>
-                        
-                        <div className="input-block">
-                            <label htmlFor="week_day">Dia da semana</label>
-                            <input type="text" id="week_day"/>
-                        </div>
+                        <Input name="subject" label="Matéria"/>
 
-                        <div className="input-block">
-                            <label htmlFor="time">Hora</label>
-                            <input type="text" id="time"/>
-                        </div>
+                        <Input name="week_day" label="Dia da semana"/>
+
+                        <Input type="time" name="time" label="Hora"/>
                     </form>
                 </PageHeader>   
 
                 <main>
-                    <TeachersItem />
-                    <TeachersItem />
-                    <TeachersItem />
                     <TeachersItem />
                 </main>             
             </div>
